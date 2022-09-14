2e7942dad367df66997f7ec5ab42417e384d8622
yannick mutombo <yannickmutombo@yannicks-MacBook-Air.local>
2022-09-15 01:48:26 +0200

    putch

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-#include <unistd.h>
 #include "main.h"
-
 /**
- * main - Check description
- * Description: It prints the word _putchar, followed by a new line.
- * Return: 0.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char word[9] = "_putchar";
-	int i;
+	char *sh = "_putchar";
 
-	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
 
 	return (0);
