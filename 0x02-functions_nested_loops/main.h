89f8314747c83c31e4109601613ad3fa604cab27
yannick mutombo <yannickmutombo@yannicks-MacBook-Air.local>
2022-09-15 02:37:38 +0200

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,11 @@
+#ifindef MAIN_H
+#define MAIN_H
 
+/*
+ * File: MAIN.h
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
 
 int _putchar(char c);
 void print_alphabet(void);
