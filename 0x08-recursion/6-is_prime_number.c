1414fbd0ec2ec5edf2dd96118bbf3d086a22ad05
yannick mutombo <yannickmutombo@yannicks-MacBook-Air.local>
2022-09-28 23:34:17 +0200

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,31 @@
 #include "main.h"
-#include <stdio.h>
-
-int check_prime(int n, int i);
-
+int prime_checker(int n, int i);
 /**
-  * is_prime_number - Returns if a number is prime
-  * @n: the number to be checked
-  *
-  * Return: integer value
-  */
+ * is_prime_number - executes prime_checker
+ * @n: input to check
+ * Return: Always 0 (Success)
+ */
 int is_prime_number(int n)
 {
-	return (check_prime(n, 1));
+	if (n <= 1)
+		return (0);
+	else if (prime_checker(n, n / 2) > 0)
+		return (1);
+	return (0);
 }
 
 /**
-  * check_prime - Check if number is prime
-  * @n: the number to be checked
-  * @i: the iteration times
-  *
-  * Return: 1 for prime or 0 co
+ * prime_checker - checks for prime
+ * @n: input to check
+ * @i: n / 2, then passes to i - 1, checks if greater than 0
+ * Return: prime check
+ */
+int prime_checker(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	else
+		return (prime_checker(n, i - 1));
+}
