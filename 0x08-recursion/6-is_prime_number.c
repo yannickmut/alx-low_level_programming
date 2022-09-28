f54f7cb88818da829c8737dc954f70c129ae73f6
yannick mutombo <yannickmutombo@yannicks-MacBook-Air.local>
2022-09-28 23:24:56 +0200

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+int check_prime(int n, int i);
+
+/**
+  * is_prime_number - Returns if a number is prime
+  * @n: the number to be checked
+  *
+  * Return: integer value
+  */
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+
+/**
+  * check_prime - Check if number is prime
+  * @n: the number to be checked
+  * @i: the iteration times
+  *
+  * Return: 1 for prime or 0 co
