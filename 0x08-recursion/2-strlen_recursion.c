8825ebd6d28e3b8a1e7e79a775da4d9cabc7f48b
yannick mutombo <yannickmutombo@yannicks-MacBook-Air.local>
2022-09-28 23:30:29 +0200

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,14 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-  * _strlen_recursion - Calculate the length of a string
-  * @s: the string to count
-  *
-  * Return: integer value
-  */
+ * _strlen_recursion - note: no _putchar in local test
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
-
-	s++;
-	return (_strlen_r
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
