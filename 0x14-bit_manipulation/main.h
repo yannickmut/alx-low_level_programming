491abef43d493a3d494d1dd521443af0a53f8aa2
yannick mutombo <yannickmutombo@yannicks-MacBook-Air.local>
2022-10-30 13:42:31 +0200

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,4 +12,4 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
-#endif /* HOLBERTON_H */
+#endif 
