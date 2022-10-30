26b81152e59cfcb8810df006265aa498e642bd73
yannick mutombo <yannickmutombo@yannicks-MacBook-Air.local>
2022-10-30 13:25:20 +0200

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
+
 /**
- * print_binary - Entry Point
- * @n: dec input
- * Return: 0
- */
+  * print_binary - Prints the binary representation of a number
+  * @n: The number to representing in binary
+  *
+  * Return: Nothing
+  */
 void print_binary(unsigned long int n)
 {
-	int i = 0, count, k, temp;
-
 	if (n == 0)
 	{
-		printf("0");
+		_putchar('0');
 		return;
 	}
 
-	temp = n;
+	_divide(n);
+}
 
-	while (temp != 0)
-	{
-		i++;
-		temp = temp >> 1;
-	}
+/**
+  * _divide - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+		return;
 
-	for (count = i - 1; count >= 0; count--)
-	{
-		k = n >> count;
-		if (k & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
 }
